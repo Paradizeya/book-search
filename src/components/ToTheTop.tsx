b2b7e8c4866041f4526fee8379ef365779d8b384
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-09-06 21:43:01 +0300

    To the Top button added

---
+++ src/components/ToTheTop.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ToTheTop = () => {
+  return (
+    <button
+      onClick={() => {
+        window.scrollTo(0, 0);
+      }}
+      className="toTheTop-button"
+    >
+      <svg
+        viewBox="0 0 24.00 24.00"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        stroke="#ffffff"
+        transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+            d="M15 11L12 8M12 8L9 11M12 8V16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+            stroke="#ffffff"
+            strokeWidth="1.44"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>{" "}
+        </g>
+      </svg>
+    </button>
+  );
+};
+
+export default ToTheTop;
