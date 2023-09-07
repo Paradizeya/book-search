5f93fa843563503a4cdebda284168e65d3966113
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-09-07 23:00:36 +0300

    HashRouter for git

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./styles/index.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
